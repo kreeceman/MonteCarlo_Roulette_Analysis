d141e2102ee3a8203ee41c4522ed70326daaf981
kreeceman <117618407+kreeceman@users.noreply.github.com>
2026-07-06 23:46:25 +0000

    Created the bet class and number info

--- EU Roulette.py
+++ EU Roulette.py
@@ -17,3 +17,26 @@ class RouletteWheel:
 
     def spin(self):
         return random.choice(self.wheel)
+#Create the properties
+
+number_info = {}
+red_numbers = {1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36}
+for n in range(1, 37):
+
+    number_info[n] = {
+        "color": "red" if n in red_numbers else "black",
+        "dozen": ((n - 1) // 12) + 1,
+        "column": ((n - 1) % 3) + 1,
+        "even": n % 2 == 0,
+        "high": n >= 19
+    }
+number_info[0] = {"color": "green"}
+number_info["00"] = {"color": "green"}
+
+# Create Bet Class
+class Bet:
+
+    def __init__(self,name,amount):
+
+        self.name=name
+        self.amount=amount
